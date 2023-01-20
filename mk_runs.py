3bc56c1c08eaffae1b88da5306034c5269e6a371
Peter Teuben <teuben@gmail.com>
2023-01-20 18:22:35 -0500

    one more

--- mk_runs.py
+++ mk_runs.py
@@ -26,15 +26,19 @@ on['J004056.01'] = [44710, 44711, 44712, 44714, 44715, 44716, 44718, 44719, 4472
 
 on['J011646.77'] = [ 52696, 52697, 52698, 52700, 52701, 52702]
 
+on['J014341.2'] = [ 51650, 51651, 51652, 51654, 51655,
+                    51656, 51658, 51659, 51660, 52706,
+                    52707, 52708, 52710, 52711, 52712]
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1['J004056.01'] = ""
 pars1['J011646.77'] = ""
-
+pars1['J014341.2']  = ""
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
 pars2['J004056.01'] = ""
 pars2['J011646.77'] = ""
+pars2['J014341.2']  = ""
 
 runs.mk_runs(project, on, pars1, pars2)
